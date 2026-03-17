7761cd095bee6d71ae2e12c7e0d86d08d408ddee
mimiokougbo-sketch <mimiokougbo@gmail.com>
2026-03-17 10:48:40 +0000

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -61,8 +61,5 @@ Make each concept different, practical, and easy to read.`
     console.error(error);
     return res.status(500).json({ error: "Something went wrong" });
   }
-}
-
-
-
- 
+};
+     
